71320d60292be7e819140330a216af41ba57d8c2
epastorizalorenzo <epastorizalorenzo@atanasoff26.danielcastelao.org>
2016-04-19 09:29:14 +0200

    commit github

---
+++ src/boletin25_2/Boletin25_2.java
@@ -0,0 +1,15 @@
+package boletin25_2;
+
+/**
+ *
+ * @author epastorizalorenzo
+ */
+public class Boletin25_2 {
+
+    
+    public static void main(String[] args) {
+        Ventana obx = new Ventana();
+        obx.setVisible(true);
+    }
+    
+}
